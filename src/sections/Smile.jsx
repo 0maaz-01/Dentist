7c8f9f28021c26713aa91af2fd4f70bf8a38e4c3
Mohammed Maaz Rayeen <mmaaz0510@gmail.com>
2025-07-12 14:14:01 +0530

    Add files via upload

---
+++ src/sections/Smile.jsx
@@ -0,0 +1,20 @@
+const Smile = () => {
+
+  return (
+    <section id = "#blog" className="relative 2xl:flex 2xl:justify-center 2xl:items-center ">
+
+        <div className="w-full h-screen bg-no-repeat bg-cover bg-center     max-h-[150px]     2xl:max-w-[3000px]       lg:max-w-[1536px] md:max-w-[1536px] bg-[url('/images/Smile.jpg')]">
+
+          
+
+            <div className="flex items-center justify-center h-full text-white  playfair-font ">
+                  
+            </div>
+        </div>
+
+    </section>
+
+  );
+};
+
+export default Smile;
