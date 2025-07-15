00c43a7e4910d54402ab575dd5e8a1cc87c8e4d6
Mohammed Maaz Rayeen <mmaaz0510@gmail.com>
2025-07-15 16:16:59 +0530

    Add files via upload

---
+++ src/sections/Map.jsx
@@ -0,0 +1,20 @@
+
+
+const Map = () => {
+
+  return (
+    <section id = "#map" className="relative 2xl:flex 2xl:justify-center 2xl:items-center ">
+
+        <div className="w-full h-screen bg-no-repeat bg-cover bg-center  2xl:max-h-[1300px] 2xl:max-w-[3000px]   max-h-[700px]   sm:max-h-[1000px]   lg:max-w-[1536px]    md:max-w-[1536px] 
+                  bg-[url('/images/Map.jpg')]   ">
+          
+
+
+        </div>
+
+    </section>
+
+  );
+};
+
+export default Map;
