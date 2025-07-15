3740bc028c967fa41f4d42e0b6727f917704112a
Mohammed Maaz Rayeen <mmaaz0510@gmail.com>
2025-07-15 16:07:31 +0530

    Add files via upload

---
+++ src/sections/Footer.jsx
@@ -0,0 +1,15 @@
+import FooterDown from "./FooterDown"
+import FooterUp from "./FooterUp";
+
+const Footer = () => {
+
+  return (
+    <section id = "#zero" className="">
+          <FooterUp/>
+          <FooterDown/>
+    </section>
+
+  );
+};
+
+export default Footer;
