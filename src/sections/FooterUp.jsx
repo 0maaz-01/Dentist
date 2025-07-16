0c6b7dd0dd006217a3b13d1808034a58a097b81f
Mohammed Maaz Rayeen <mmaaz0510@gmail.com>
2025-07-16 22:56:44 +0530

    Add files via upload

---
+++ src/sections/FooterUp.jsx
@@ -0,0 +1,17 @@
+
+
+const FooterUp = () => {
+
+  return (
+    <section id = "#footerup" className="relative 2xl:flex 2xl:justify-center 2xl:items-center      ">
+
+        <div  className="bg-black/50   flex items-center justify-center  w-[100vw]  max-w-[1800px]   pt-[100px]  pb-[100px]">
+            <img   alt="Pure Harmony Dental logo"   className="w-70" src = "/images/Blog1.png"/>
+        </div>
+
+    </section>
+
+  );
+};
+
+export default FooterUp;
