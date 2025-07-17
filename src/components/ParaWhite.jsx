bb8f3cf8dde1f9145e3ee158ef40fd7299f4c39c
Mohammed Maaz Rayeen <mmaaz0510@gmail.com>
2025-07-17 12:35:11 +0530

    Add files via upload

---
+++ src/components/ParaWhite.jsx
@@ -0,0 +1,9 @@
+
+
+const ParaWhite = ({content}) => {
+    return (
+        <div  className="font-sans      text-[#ffff]         m-4  text-base    sm:m-4 sm:mb-6 sm:text-lg    md:text-xl    lg:text-[22px] lg:m-4         2xl:text-2xl 2xl:lg:m-8">{content}</div>
+    )
+}
+
+export default ParaWhite;
